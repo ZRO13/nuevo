a7610b70d18eb91b217b5816c0eb239e4f306fa2
zero13 <juliocoloma183@gmail.com>
2022-07-24 00:56:54 -0500

    USANDO COMMIT

---
+++ primer.js
@@ -0,0 +1,292 @@
+
+// let colorpared = prompt("que color es tu pared");
+// alert("tus paredes son "+colorpared )
+// let plata = prompt("¡eh pibe cuanta plata tenes");
+// alert ("tengo "+plata)
+// let nombre = prompt("como tellamas?");
+// if(nombre = petter){alert("sos un care piña")};
+// 	if else (nombre = manuel){alert("tu nombre vale v")}
+// let numero1 = 10;
+// let numero2 = 10;
+// let numero3 = 250;
+// let numero4 = 44;
+// sop = (numero1==numero4)||(!(numero3<numero4))
+// document.write(sop)
+// //
+// `// 
+// let nombre ="julio";
+// alert(`eh ${nombre} sos un crak`)
+
+// numero1 = 10;
+// numero2 = "20";
+// conca  = numero1.concat("numero2");
+// // document.write(conca)`
+// let numero1=100;
+
+// let numero2=200;
+// document.write(numero1<=numero2)
+
+// let numero1=100;
+// numero1 **=5;
+// document.write(numero1)
+// let numero1=100;
+// numero1 =5;
+// document.write(numero1)
+
+// let kulio = prompt("cual es nombre mijin?");
+// alert("su nombre es bacano felicidades señ@r "+kulio)
+
+// let numero = prompt ("indiqueme un numero");
+// numero %=100;/*el signo se ddebe de poner alado del igual y dejando un espacio del nombre de la variable*/
+// alert("su resultado es "+numero)
+
+// let numero1 ,numero8 , numero10;/*para inicializar algunas variables*/
+// numero1=100;
+// numero8=200;
+// numero10=300;
+// alert(numero1)
+// alert(numero8)
+// alert(numero10)
+
+// let numero=20;
+// let nombre="julio"
+// alert(nombre+numero)
+
+// n1=100;
+// resultado=-n1;//se le pone el menos adelante de la variable le ponemo - si lo queremnos combertir a negativo y + para positivo
+// alert(resultado)
+// if (1400<5000) {alert("buenardo")}
+
+// nombre="teodoro";
+// if (nombre=="teodoro") {alert("te llaMAS teodoro yo tambien loco"); }
+// else if (nombre=="pedro") {alert("te llamas igual q mi tio");}
+
+
+
+
+
+// let nombre = prompt("cual es su nombre?");
+// if (nombre=="julito") {alert("eres ekl usuario numero1000");}
+// else if (nombre=="pablito") {alert("pablo volviste a entrar a nuestra pagina a buscar algo");s}
+
+
+
+
+
+// let nombre = prompt("usuario");
+// if (nombre=="luis") {alert("tu nombre es buenardo");}
+// 	else if (nombre=="ederson") {alert("tu nombre es brasileño");}
+// else {alert("tu nombre es otro cojo ");}
+
+
+// n1 = 1000
+// n2 = 2000
+// function vaba(n1,n2)
+// {return n1+n2}
+// console.log(vaba(n1,n2))
+
+
+
+
+
+
+// let k= 500
+// let E= 1000
+ 
+//  function funcion(k,E) {return k+E}
+//  console.log(funcion(k,E))
+
+
+
+
+
+
+
+
+
+
+//  let julio =18
+//  let luis = 18
+//  function edadTotal(julio,luis) { return julio+luis}
+//  console.log(edadTotal(julio,luis) )
+
+
+
+
+// let b =50000000000
+// let a=10000000
+// function res (a,b) {return a%b}
+// console.log(res(a,b))
+//  propiedade peso modelo 
+//  funcion avanzar detenerse prender apagar
+
+
+//  const car = {tipo:"mercedes",
+//  modelo:"z500",
+//  peso:"10 toneladas",
+//  año:2017}; // este es un objeto o diccionario
+//   car.modelo="5hjnm";
+//      console.log(car.tipo)     // CUANDO DECLARAMOS OBJETOS LE PONEMOS CONTS CONSTANTE Y SE PUEDE PONER DISTINTOS TIPOS DE DATOS INT FLOATS STRINGS
+
+
+// let numeros=[20,520,50,5245,474];
+// document.write(numeros[2])
+const eme ={nombre:"xulio",modelo:"jnde41",edad:14,enemigos:["agua","perros"],};
+eme.nombre="milin";
+console.log(eme)
+
+
+
+
+// // console.log(car.año)
+// // let jn[tipo:"chevrolet";año:"20220";peso"1000kg"]
+// // console.log(jn[2])
+// const karan = {nombre:"mulo",edad:"41",peso:"12kg"};
+// karan.peso="50kg";
+// delete karan.nombre;
+// console.log(karan);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const kaka={nombre:"julio",edad:18,nacionalidad:"ecu",enemigos:["puca","puco"],};
+// console.log(kaka.edad);
+// console.log(kaka["nombre"]);
+// console.log(kaka.enemigos[1]);
+// console.log(kaka["enemigos"][1]);
+
+// const pepe={nombre:"jose",edad:18,nacionalidad:"ecu",color:["blanco","mestizo"]};
+// console.log(pepe.nombre);
+// console.log(pepe["color"][1]);
+// console.log(pepe.color[0]);
+
+const persona={nombre:"pedro",edad:"19",nacionalidad:"ecu",amigos:["juan","pedro","kaka"],};
+console.log(persona.edad);
+console.log(persona["amigos"][0]);
+console.log(persona.amigos[1]);
+console.log(persona["amigos"][2]);
+
+const perro ={nombre:"puca",edad:7,nacionalidad:"ecu",peso:"5kg",
+otros:{alimentos:{favoritos:{
+                       comida:"carnitas",
+                       merienda:"pollo",
+                       almuerzo:"llema",
+}}}};
+console.log(perro.otros.alimentos.favoritos.comida)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
